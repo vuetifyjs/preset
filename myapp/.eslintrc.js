0eb093c45ca89405bb11c1c7e3609a9b23babd92
Mert <mertpolat.98@hotmail.com>
2020-08-17 19:15:51 +0200

    init

---
+++ myapp/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: 'vuetify',
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+  },
+}
